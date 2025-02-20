d335d8d4de665b8339f3e135a7892c2916e0f877
Aloysius J Kolapran <aloysiusjkolapran001@gmail.com>
2025-02-20 12:08:58 +0530

    header change

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,10 +1,13 @@
 function Header() {
-    return <div className="flex justify-between">
-        <h1>logo</h1>
-        <ul className="flex gap-5">
-            <li>log-in</li>
-            <li>sign-in</li>
-        </ul>
+  return (
+    <div className="flex justify-between">
+      <h1>logo</h1>
+      <ul className="flex gap-5">
+        <li>log-in</li>
+        <li>sign-in</li>
+        <li>qwerty</li>
+      </ul>
     </div>
+  );
 }
 export default Header;
